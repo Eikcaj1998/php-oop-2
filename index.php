1371818b56d89846723240f5c69d2ba30083d9ae
Eikcaj.1998 <jw220998@gmail.com>
2022-09-12 15:41:44 +0200

    classes

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+ require_once __DIR__ . '/classes/Product.php';
+ require_once __DIR__ . '/classes/Food.php';
+ require_once __DIR__ . '/classes/Games.php';
+ require_once __DIR__ . '/classes/RegisteredCard.php';
+ require_once __DIR__ . '/classes/Client.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
